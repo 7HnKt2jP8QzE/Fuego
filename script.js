d3a5b056651693bab8cd018cccf23c2f617d5271
7HnKt2jP8QzE <94395211+7HnKt2jP8QzE@users.noreply.github.com>
2024-07-28 17:30:41 +0200

    up

---
+++ script.js
@@ -0,0 +1,19 @@
+const listInfo = document.querySelector('.list-info');
+const nextBtn = document.querySelector('.next-btn');
+const prevBtn = document.querySelector('.prev-btn')
+
+let index = 0; 
+
+nextBtn.addEventListener('click', () =>{
+
+    index = (index < 3) ? index + 1 : 3;
+    
+    listInfo.style.transform = `translateY(${index * -25 }%)`;
+});
+
+prevBtn.addEventListener('click', () =>{
+
+    index = (index > 0) ? index - 1 : 0;
+    
+    listInfo.style.transform = `translateY(${index * - 25 }%)`;
+});
